ba0ceece564c8ee8179d378cd1ee1fe690dd9c2a
yiguihai <18776473475@163.com>
2018-01-28 20:30:43 +0800

    fix android 8.1 run shell script

--- Client/检测.sh
+++ Client/检测.sh
@@ -1,3 +1,8 @@
 #!/system/bin/sh
 
-"${0%/*}"/TSocks
+#系统命令默认路径
+BIN="/system/bin"
+#当前执行目录路径
+DIR="`pwd`"||DIR="`$BIN/busybox pwd`"||DIR="`$BIN/toybox pwd`"||DIR="${0%/*}" > /dev/null 2>&1||exit $?
+
+${BIN}/sh ${DIR}/TSocks -c ${BIN} ${DIR}
