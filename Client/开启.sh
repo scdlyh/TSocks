2f98b7332df96608fa4c7c497e1bff0c8053f11b
yiguihai <18776473475@163.com>
2018-02-26 06:19:20 -0600

    Update 开启.sh

--- Client/开启.sh
+++ Client/开启.sh
@@ -1,8 +1,6 @@
 #!/system/bin/sh
 
-#系统命令默认路径
-BIN="/system/bin"
 #当前执行目录路径
 DIR="`pwd`"||DIR="`$BIN/busybox pwd`"||DIR="`$BIN/toybox pwd`"||DIR="${0%/*}" > /dev/null 2>&1||exit $?
 
-${BIN}/sh ${DIR}/TSocks -s ${BIN} ${DIR}
+${BIN}/sh ${DIR}/TSocks -s ${DIR}
