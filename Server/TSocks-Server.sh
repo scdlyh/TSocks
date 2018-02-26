8a9d0530e1e384c2a9fdf3304ded4723db4a4e35
yiguihai <18776473475@163.com>
2018-02-25 21:21:57 -0600

    Update TSocks-Server.sh

--- Server/TSocks-Server.sh
+++ Server/TSocks-Server.sh
@@ -275,7 +275,8 @@ download_files() {
         fi
         download "${shadowsocks_libev_file}.tar.gz" "${shadowsocks_libev_url}"
     elif [ "${selected}" == "2" ]; then
-        get_gost_ver
+        #get_gost_ver
+        get_gost_ver='v2.3';
         if is_64bit; then
         gost_file="gost_$(echo ${gost_ver} | sed -e 's/^[a-zA-Z]//g')_linux_amd64"
         else
